5e1abdb91927f798a797935b779a8ee865e5b6a4
chintu56 <155710174+chintu56@users.noreply.github.com>
2024-10-01 10:34:48 +0530

    Update mid point of the line.py

--- mid point of the line.py
+++ mid point of the line.py
@@ -4,4 +4,13 @@ Chandru live in the same locality. A new student Dinesh joins their class and he
 Binoy told Dinesh that his house is at the midpoint of the line joining Ajay's house and Chandru's house. Dinesh was puzzled. Can you help Dinesh out? Given the coordinates of the 2 end points of a line (x1,y1) and (x2,y2), 
 write a python program to find the midpoint of the line. Input Format: Input consists of 4 integers. The first integer corresponds to x1 . The second integer corresponds to y1. The third and fouth integers correspond to x2 and y2 respectively.
 Output Format: Refer Sample Input and Output for exact formatting specifications. [All floating point values are displayed correct to 1 decimal place]
+Input (stdin)
+2
+4
+10
+15
+ 
+Output (stdout)
+Binoy's house is located at (6.0,9.5)
+
 """
